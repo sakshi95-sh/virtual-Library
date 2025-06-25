9e67d017b3e0cef10fc3e5904a5a4baef5927254
Sakshi Sharma <sakshi02feb@gmail.com>
2025-06-24 22:35:37 -0400

    UPDATED VERSION

--- library.js
+++ library.js
@@ -1,130 +1,204 @@
-//Library 
+//Library
 class Library {
   constructor() {
     this.catalog = [];
+    this.historyLog = [];
   }
-  addBook(bookDetails)
-  {
-    this.catalog.push(bookDetails);
-  }
+
+  //get all books
   get getAllBook() {
     return this.catalog;
   }
-  findBookByTitle(bookTitle) {
-  let matchedBook=  this.catalog.find(book => {
-      return book.bookName === bookTitle
-    })
-    if (matchedBook) {
-     return console.log(`Book: ${matchedBook.bookName} -  Author: ${matchedBook.authorName} Status: Available`)
+  //⁠insert a new book 
+  addBook(bookDetails) {
+    this.catalog.push(bookDetails);
+    console.log(this.catalog);
+    return this.catalog
+  }
+  //delete a book
+  remove(bookName) {
+   const matchedBookIndex=  this.catalog.findIndex(val => {
+      return val.bookName === bookName
+   })
+    this.catalog.splice(matchedBookIndex, 1)
+    console.log(this.catalog)
+  
+    return this.catalog
+  }
+  //borrow a book
+  borrow(bookName, userDetails) {
+    //To Match The Books Details 
+    const matchedBookIndex = this.catalog.findIndex(val =>
+    { return val.bookName === bookName })
+    if (matchedBookIndex != -1 && this.catalog[matchedBookIndex].availability === true) {
+      const currentDate = new Date();
+      const borrowed = {
+        name: userDetails.name,
+        id: userDetails.id,
+        email:userDetails.email,
+        action: "borrowed",
+        bookName: bookName,
+        Date: `${currentDate.getMonth()+1}/${currentDate.getDate()}/${currentDate.getFullYear()+1}`,
+      }   
+      this.catalog[matchedBookIndex].availability = false;
+      console.log(borrowed)
+      return this.historyLog.push(borrowed);
     }
-    else return console.log(`This book is not available`)
+    else return console.log(`This Book is not currently Available`);
+  }
+  //Return a  Book
+  return(bookName,userDetails) {
+  const matchedBookIndex = this.catalog.findIndex(val =>
+    { return val.bookName === bookName })
+      const currentDate = new Date();
+      const borrowed = {
+        name: userDetails.name,
+        id: userDetails.id,
+        email:userDetails.email,
+        action: "Returned",
+        bookName: bookName,
+        Date: `${currentDate.getMonth()+1}/${currentDate.getDate()}/${currentDate.getFullYear()+1}`,
+      }   
+      this.catalog[matchedBookIndex].availability = false;
+      console.log(borrowed)
+      return this.historyLog.push(borrowed);
+  }
+  history(id) {
+    if (id === null) {
+            console.log(this.historyLog)
+      return this.historyLog
+    }
+    else {
+      console.log(this.historyLog)
+      const userHisory = this.historyLog.filter((val) => {
+        return val.id === id
+      })
+      console.log(userHisory)
+      return userHisory
+    }
+  }
+  // Get Available Book
+  availableBook() {
+    const availableBooks=  this.catalog.filter(val => {
+      return val.availability === true
+    })
+    return availableBooks.forEach(book =>
+      console.log(`Book: ${book.bookName}, Author: ${book.authorName}, Available: ${book.availability}`));
   }
 }
 
-let firstBook = {
+class User{
+  constructor(name, email, id,role,library) {
+    /* To Call Parent Constructor */
+    this.name = name;
+    this.email = email;
+    this.id = id;
+    this.role = role;
+    this.library = library;
+
+  }
+  borrow(bookName) {
+    console.log("USER ID", this.id)
+   this.library.borrow(bookName,this);
+  }
+  return(bookName) {
+    this.library.return(bookName,this);
+  }
+  viewAvailableBooks() {
+    this.library.availableBook()
+  }
+  history() {
+    this.library.history(this.id)
+  }
+}
+
+class Admin extends User{
+  history(id=null) {
+    console.log("ADMIN HISTORY")
+    return this.library.history(id);
+  }
+  addBook(bookDetails) {
+    return this.library.addBook(bookDetails)
+  }
+
+  remove(bookName) {
+    return this.library.remove(bookName)
+  }
+
+}
+
+const firstBook = {
   bookName: 'Atomic Habits',
   authorName: 'James Clear',
   availability: true,
 };
-let secondBook = {
+
+const secondBook = {
   bookName: 'Deep Work',
   authorName: 'Cal Newport',
   availability: true,
 };
-                
+
+const thirdBook = {
+  bookName: 'The Pragmatic Programmer',
+  authorName: 'Andrew Hunt & David Thomas',
+  availability: true,
+};
+
+const fourthBook = {
+  bookName: 'Show Your Work!',
+  authorName: 'Austin Kleon',
+  availability: true,
+};
+
+const fifthBook = {
+  bookName: 'Can’t Hurt Me',
+  authorName: 'David Goggins',
+  availability: true,
+};
+
+const sixthBook = {
+  bookName: 'So Good They Can’t Ignore You',
+  authorName: 'Cal Newport',
+  availability: true,
+};
+
+const seventhBook = {
+  bookName: 'Clean Code',
+  authorName: 'Robert C. Martin',
+  availability: true,
+};
+
+const eighthBook = {
+  bookName: 'Mindset: The New Psychology of Success',
+  authorName: 'Carol S. Dweck',
+  availability: true,
+};
+
 const library = new Library;
 library.addBook(firstBook);
 library.addBook(secondBook);
-// console.log(library.findBookByTitle('Deep Work'))
-// const bookDetails = [];
-// bookDetails.push(new Library('Atomic Habits', 'James Clear', true));
-// bookDetails.push(new Library('Deep Work', 'Cal Newport', true));
-// bookDetails.push(new Library('The Pragmatic Programmer', 'Andrew Hunt & David Thomas', true));
-// bookDetails.push(new Library('Show Your Work!', 'Austin Kleon', true));
-// bookDetails.push(new Library('Can’t Hurt Me', 'David Goggins', true));
-// bookDetails.push(new Library('So Good They Can’t Ignore You', 'Cal Newport', true));
-// bookDetails.push(new Library('Clean Code', 'Robert C. Martin (Uncle Bob)', true));
-class UserDetails extends Library{
-   #userBorrowed = [];
-  constructor(userName, id) {
-    this.userName = userName;
-    this.id = id;
-  }
-  borrowBook(bookname) {
-    for (let books of bookDetails)
-    {
-      if (books.bookName === bookname && books.availability != true) {
-       return console.log(`This ${bookname} is currently unavailable`)
-      }
-      else if(books.bookName === bookname) {
-        books.availability = false;
-       this.#userBorrowed.push({
-          title: books.bookName,
-          author: books.authorName
-        });
-        return console.log(`Book has been issued ${books.bookName} - ${books.authorName}`)
-      }
-    }
-    return console.log("We don't have this book in Library")
-  }
-  myBorrowed() {
-    if (this.#userBorrowed.length === 0)
-    {
-      return console.log("You haven't borrowed any books.");
-    }
-  this.#userBorrowed.forEach((book,index) => {
-      console.log(`${index+1}. ${book.title} -${book.author} `)
-    });
-  }
-  returnBook(name) {
-    for (let book of bookDetails) {
-      if (book.bookName==name) {
-        book.availability = true;
-       this.#userBorrowed =this.#userBorrowed.filter(ele => !new RegExp(name).test(ele))
-        return `Book returned successfully`
-      }
-    }
-  }
-  checkBookAvailabitity(status = false) {
-    if (status) {
-     return bookDetails
-    } else
-      return bookDetails.filter(books => books.availability === true);
-    
-  }
-}
+library.addBook(thirdBook);
+library.addBook(fourthBook);
+library.addBook(eighthBook);
+library.addBook(sixthBook);
+const user1 = new User('abc', 'abd@xyz.com', 12,'user', library);
+user1.borrow('Mindset: The New Psychology of Success', user1)
+user1.return('Mindset: The New Psychology of Success', user1)
+user1.viewAvailableBooks();
+user1.history(12)
 
-class Admin extends UserDetails{
-  allBooks() {
-    const bookDetails = this.checkBookAvailabitity(true)
-    for (let books of bookDetails) {
-      console.log(`Book: ${books.bookName} Author: ${books.authorName} - ${books.availability?'Available':'Borrowed'}`)
-    }
-    
-    }
-}
-class Member extends UserDetails {
-   #userBorrowed = [];
-  constructor(userName, id) {
-    this.userName = userName;
-    this.id = id;
-  }
-  allBooks() {
-    //Only available books details
-    const bookDetails = this.checkBookAvailabitity()
-    for (let books of bookDetails) {
-     console.log(`Book: ${books.bookName} -  Author: ${books.authorName} - 'AVAIABLE'}`);
-    }
-    
-  }
-}
+const user2 = new User('xyz', 'xyz@xyz.com', 14, 'user',library);
+user2.borrow('So Good They Can’t Ignore You', user2)
+user2.return('So Good They Can’t Ignore You', user2)
+user1.viewAvailableBooks();
+user1.history(14)
 
-const user1 = new UserDetails('Joel', 124);
-const user2 = new UserDetails('Ben', 1242);
-const admin = new Admin('Admin', 9898);
-const membe1 = new Member('Joel', 124);
-const member2 = new Member('Cindy',124);
+const user3 = new User('user', 'admin@xyz.com', 14,'user' , library);
 
 
+const admin = new Admin('Admin', 'admin@xyz.com', 18, 'admin', library);
+admin.history();
 
-
+admin.addBook(fifthBook);
+admin.remove('Atomic Habits');
